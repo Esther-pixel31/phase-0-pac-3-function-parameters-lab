c2e44d8c962d3f03e1802e6ab63acc2a202d1ba4
EstherMutua <esthermutua16@gmail.com>
2025-02-28 17:14:52 +0300

    added index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+
+function introductionWithLanguageOptional(name, language = "JavaScript"){
+ console.log(`Hello, my name is ${name}, and I am learning to program in ${language}.`);
+}
+
+introductionWithLanguageOptional("Gracie" , "Python");
+introductionWithLanguageOptional("Gracie");
